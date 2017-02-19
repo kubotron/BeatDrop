c706386343b2e1ef0c615dbaeda6fca79b9cc30c
Jakub <jakubludma@gmail.com>
2017-02-19 17:59:11 +0100

    modified install script

--- skydrop/inst.sh
+++ skydrop/inst.sh
@@ -3,5 +3,5 @@ cd Release
 ../utils/build/pre_build.sh
 make
 ../utils/build/post_build.sh
-cp SKYDROP.FW /Volumes/NO\ NAME/
-diskutil unmount /Volumes/NO\ NAME/
+cp SKYDROP.FW /Volumes/BIBEEP/
+diskutil unmount /Volumes/BIBEEP/
